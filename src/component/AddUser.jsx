db010b83186981eafca0effdee7c7adc1b08f2c4
Majid Ali <lahmajidali@gmail.com>
2023-01-18 09:23:59 -0800

    achieve the useRef approach

--- src/component/AddUser.jsx
+++ src/component/AddUser.jsx
@@ -1,4 +1,4 @@
-import React, {useState , Fragment} from 'react'
+import React, {useState , Fragment , useRef} from 'react'
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from "./AddUser.module.css"
@@ -10,23 +10,31 @@ import ErrorModal from '../UI/ErrorModal';
 function AddUser(props) {
 
    // to get and store values
-  const [enteredUserName , setEnteredUserName] = useState("");
-  const [enteredUserAge , setEnteredUserAge] = useState("");
+
+  // const [enteredUserName , setEnteredUserName] = useState("");
+  // const [enteredUserAge , setEnteredUserAge] = useState("");
+  
   // to manage error message
   const [error , setError ] = useState()
+
+  // We use useRef to interect directly with the DOM-Nodw/ React-Element
+  const userNameRef = useRef();
+  const userAgeRef = useRef();
 
 
 
   const AddUserHandler = (event)=>{
     event.preventDefault();
-    if(enteredUserName.trim().length === 0 || enteredUserAge.trim().length === 0 ){
+    console.log(userNameRef.current.value) 
+    // if(enteredUserName.trim().length === 0 || enteredUserAge.trim().length === 0 ){
+    if(userNameRef.current.value === ""|| userAgeRef.current.value === 0 ){
       setError({
         title : "Invalid input !",
         message : "Please enter a valid name and age (non-empty values)"
       })  
       return; 
     } 
-    if(+enteredUserAge < 1){ 
+    if(+userAgeRef.current.value < 1){ 
     setError({
       title : "Invalid Age",
       message : "Please enter a valid age(> 0)"
@@ -34,26 +42,30 @@ function AddUser(props) {
     return 
     }
     const userData = {
-      name : enteredUserName, 
-      age : enteredUserAge
+      // name : enteredUserName,
+      // age : enteredUserName, 
+      name : userNameRef.current.value,
+      age : userAgeRef.current.value
     }
     props.newData(userData)
+    userNameRef.current.value = ""
+    userAgeRef.current.value = ""
     // console.log(enteredUserName , enteredUserAge)
-    setEnteredUserName("")
-    setEnteredUserAge("")
+    // setEnteredUserName("")
+    // setEnteredUserAge("")
   }
 
 
 
-  const UserNameHandler = (e)=>{
-         setEnteredUserName(e.target.value)
-  }
+  // const UserNameHandler = (e)=>{
+  //        setEnteredUserName(e.target.value)
+  // }
 
 
-  const UserAgeHandler = (e)=>{
-    // if(enteredUserAge.length)
-    setEnteredUserAge(e.target.value)
-  }
+  // const UserAgeHandler = (e)=>{
+  //   // if(enteredUserAge.length)
+  //   setEnteredUserAge(e.target.value)
+  // }
 
   const ErrorHandler = ()=>{
     setError(null)
@@ -69,12 +81,14 @@ function AddUser(props) {
     <form className='user_form' onSubmit={AddUserHandler}>
         <div className='name_section'>
             <label   htmlFor="userName">Username</label>
-            <input   value={enteredUserName}  type="text"  id="userName" onChange={UserNameHandler}/>
+            {/* <input   value={enteredUserName}  type="text"  id="userName" onChange={UserNameHandler}/> */}
+            <input  ref={userNameRef}   type="text"  id="userName" />
         </div>
 
         <div className="age_section">
             <label  htmlFor="age">Age (Years)</label>
-            <input  value={enteredUserAge} type="number"  id="age"  onChange={UserAgeHandler}/>
+            {/* <input  value={enteredUserAge} type="number"  id="age"  onChange={UserAgeHandler}/> */}
+            <input  ref={userAgeRef} type="number"  id="age"  />
         </div><br/>
         <Button type="submit">Add User</Button>
     </form>
